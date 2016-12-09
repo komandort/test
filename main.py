d5503c44d08d6f4eae94af8414f68691e4aaba5c
pawellasota <plasota76@gmail.com>
2016-12-09 16:52:42 +0100

    fucked up everything

--- main.py
+++ main.py
@@ -15,12 +15,10 @@ def add(x,y)
 def print_table(table):
 	print(table)
 
-def erase_table(table):
-	del table
-
 if __name__ == "__main__":
 	main()
 
 #comment at end of file
 
 print("Commits are cool")
+#some fucked up things happened
